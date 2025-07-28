ed4b7e5fc0b290012c08b2f364e66c394fab53dd
XinChenQC <chenxin199261@gmail.com>
2025-07-28 19:09:14 -0400

    add package

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def test():
     num_samples = 20
     fragment = 'COc1ccc2c(c1)nc([nH]2)S(=O)Cc1ncc(c(c1C)OC)C' # SMILES
 
-
+    '''
     print('Doing linker_design')
     fragment2 = '[11*]N1CCCC1.[17*]c1ccc2c(c1)OCCO2' # SMILES
     samples = sampler.fragment_linking(fragment2, num_samples, randomness=3)
@@ -23,15 +23,18 @@ def test():
     fragment = '[17*]c1ccc2c(c1)OCCO2' # SMILES
     samples = sampler.fragment_completion(fragment, num_samples, randomness=1.2, gamma=0.3)
     print(samples)
-
+    '''
     print('Doing scaffold_decoration')
-    fragment = '[1*]c1cccc(Nc2ncnc3cc([2*])c([3*])cc23)c1' # SMILES
-    samples = sampler.fragment_completion(fragment, num_samples, gamma=0.3)
-    print(samples)
+    fragment = 'O=C(NN1CC([12*])CC1)c1cccc([13*])c1' # SMILES
+    samples = sampler.fragment_completion(fragment, num_samples, gamma=0.3,randomness=0.5, softmax_temp=0.5 )
+    print(len(set(samples)))
+    print(set(samples))
 
-    print('c1ccc(Oc2ccccc2)cc1')
-    samples = sampler.fragment_completion(fragment, num_samples, gamma=0.4)
-    print(samples)
+    print('Doing Superstructure Generation')
+    fragment = 'c1ccc(Oc2ccccc2)cc1' # SMILES
+    samples = sampler.fragment_completion(fragment, num_samples, gamma=0.4, randomness=3)
+    print(len(set(samples)))
+    print(set(samples))
 
 
 if __name__ == "__main__":
